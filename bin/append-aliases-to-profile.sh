1cd56d8998b47c32731a7def50b981a23b85eea6
Chris Walquist <cwalquist@drw.com>
2021-07-14 11:43:34 -0500

    Fixed bugs in append-aliases script

--- bin/append-aliases-to-profile.sh
+++ bin/append-aliases-to-profile.sh
@@ -1,8 +1,10 @@
-if ! grep -l 'alias adog' ~/.bash_profile >> /dev/null
+sh=`basename $SHELL`
+profile=~/.${sh}_profile
+if ! grep -l 'alias adog' $profile >> /dev/null
 then
-  sh=`basename $SHELL`
-  profile=~/.${sh}_profile
   echo >> $profile  # In case the profile doesn't end with a newline
-  cat git-aliases.sh >> $profile
+  cat `dirname ${BASH_SOURCE[0]}`/git-aliases.sh >> $profile
   echo Appended git aliases to $profile
+else
+  echo "Found 'adog' alias already in $profile, assuming the rest are there too."
 fi
